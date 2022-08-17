b19420ecc577e7b4e3a6711ad12e053306769b62
Wiynxr <mkx.dake@gmail.com>
2022-08-18 02:56:21 +0300

    Добавил товары

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userSlice from "../components/features/userSlice"
+import products from "../components/features/productsSlice"
 
 export const store = configureStore({
     reducer: {
         user: userSlice,
+        products: products
     }
 })
