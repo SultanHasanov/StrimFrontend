07b05594e62b692e241c5cbba05989ea6241d71e
Ross <rabik95@bk.ru>
2022-08-18 18:17:40 +0300

    Основа для корзины

---
+++ src/pages/Cart/CartPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CartPage.module.css'
+
+const CartPage = () => {
+    return (
+        <div className={styles.back}>
+          111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111  
+        </div>
+    );
+};
+
+export default CartPage;
