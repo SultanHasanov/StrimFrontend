503009c43caa3b8c2ea2b70e2776eb365f6ce7e3
Wiynxr <mkx.dake@gmail.com>
2022-08-21 18:23:06 +0300

    Исправил App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import { Routes, Route, Navigate} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HeaderMain from "./components/HeaderMain/HeaderMain";
 import MainContent from "./components/MainContent/MainContent";
 import SignUp from "./pages/signUp/SignUp";
 import SignIn from "./pages/signIn/SignIn";
 import Footer from "./components/Footer/Footer";
-import { useSelector } from 'react-redux';
-import MyTeam from './pages/my-team/MyTeam';
+import { useSelector } from "react-redux";
+import MyTeam from "./pages/my-team/MyTeam";
 import MainProducts from "./pages/MainProducts/MainProducts";
 import CartPage from "./pages/Cart/CartPage";
 import AllProducts from "./pages/MainProducts/Categories/AllProducts";
-import CabinetPage from './pages/Cabinet/CabinetPage';
-import CabinetChange from './pages/Cabinet/CabinetChange';
-import BuyPage from './pages/Buy/BuyPage';
-
-
-
+import CabinetPage from "./pages/Cabinet/CabinetPage";
+import CabinetChange from "./pages/Cabinet/CabinetChange";
+import BuyPage from "./pages/Buy/BuyPage";
 
 function App() {
-  const token = useSelector((state) => state.user.token)
-  const product = useSelector((state) => state.products.products)
+  const token = useSelector((state) => state.user.token);
+  const product = useSelector((state) => state.products.products);
 
-  if (!token || token === 'Неверный пароль' || token === 'Неверный логин' || token === 'Неверный логин или пароль') {
+  if (
+    !token ||
+    token === "Неверный пароль" ||
+    token === "Неверный логин" ||
+    token === "Неверный логин или пароль"
+  ) {
     return (
       <div className="App">
         <Header />
@@ -39,21 +41,22 @@ function App() {
   }
   return (
     <div className="App">
-        <Header />
-        <HeaderMain />
-        <Routes>
-          <Route path="/" element={<MainContent />} />
-          <Route path="/register" element={<Navigate to="/" />} />
-          <Route path="/auth" element={<Navigate to="/" />} />
-          <Route path="/my-team" element={<MyTeam />} />
-          <Route path="/products" element={<MainProducts />} />
-          <Route path="/cart" element={<CartPage />}></Route>
-          <Route path="/all-tovars" element={<AllProducts />} />
-          <Route path="/cabinet" element={<CabinetPage />}></Route>
-          <Route path="/cabinetchange" element={<CabinetChange />}></Route>
-          <Route path="/buy" element={<BuyPage />}></Route>
-        </Routes>
-        <Footer />
+      <Header />
+      <HeaderMain />
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        <Route path="/register" element={<Navigate to="/" />} />
+        <Route path="/auth" element={<Navigate to="/" />} />
+        <Route path="/my-team" element={<MyTeam />} />
+        <Route path="/products" element={<MainProducts />} />
+        <Route path="/cart" element={<CartPage />}></Route>
+        <Route path="/all-tovars" element={<AllProducts />} />
+        <Route path="/cabinet" element={<CabinetPage />}></Route>
+        <Route path="/cabinetchange" element={<CabinetChange />}></Route>
+        <Route path="/buy" element={<BuyPage />}></Route>
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/features/profile.Slice.js
+++ src/components/features/profile.Slice.js
@@ -7,7 +7,7 @@ const initialState = {
     error: null,
 }
 
-export const getPofileRequest = createAsyncThunk("/profile", async (userId, thunkAPI) => {
+export const getPofileRequest = createAsyncThunk("/profile", async ({userId}, thunkAPI) => {
     try {
         const response = await axios.get(`http://localhost:4000/profile/${userId}`)
         return response.data
